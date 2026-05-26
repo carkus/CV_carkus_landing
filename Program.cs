39f22743ef19e0112969d88f3191d8c1760af240
Mark Scicluna <dicarkus@hotmail.com>
2026-05-26 13:38:21 +1000

    renamed project, updated using directive across files

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
-using CarkusLanding;
+using CV_carkus_landing;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
 builder.RootComponents.Add<App>("#app");
